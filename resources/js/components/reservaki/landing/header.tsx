f25edd57b54537fd6c589add3d3b7662f41aef1b
rafawastaken <jrafaelp97@gmail.com>
2025-07-30 01:53:40 +0100

    header links

--- resources/js/components/reservaki/landing/header.tsx
+++ resources/js/components/reservaki/landing/header.tsx
@@ -1,7 +1,13 @@
 import { Navigation } from '@/components/reservaki/landing/navigation';
 import { Button } from '@/components/ui/button';
+import { SharedData } from '@/types';
+import { Link, usePage } from '@inertiajs/react';
 
 export default function Header() {
+    const { auth } = usePage<SharedData>().props;
+
+    console.log(auth);
+
     return (
         <header className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container mx-auto px-4 py-4">
@@ -11,8 +17,21 @@ export default function Header() {
                         <Navigation />
                     </div>
                     <div className="flex items-center space-x-3">
-                        <Button variant="ghost">Entrar</Button>
-                        <Button variant="hero">Área do Proprietário</Button>
+                        {auth.user ? (
+                            <Button variant="hero" asChild>
+                                <Link href={route('dashboard')}>Área do Proprietário</Link>
+                            </Button>
+                        ) : (
+                            <>
+                                <Button variant="ghost" asChild>
+                                    <Link href={route('login')}>Entrar</Link>
+                                </Button>
+
+                                <Button variant="hero" asChild>
+                                    <Link href={route('register')}>Começar a usar</Link>
+                                </Button>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -28,16 +28,19 @@ export interface SharedData {
     auth: Auth;
     ziggy: Config & { location: string };
     sidebarOpen: boolean;
+
     [key: string]: unknown;
 }
 
 export interface User {
     id: number;
     name: string;
+    lastname: string;
     email: string;
     avatar?: string;
     email_verified_at: string | null;
     created_at: string;
     updated_at: string;
+
     [key: string]: unknown; // This allows for additional properties...
 }
